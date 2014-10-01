b2c01187dbed9635535d2d1e5daf32568e59da52
JYlo <g2648@student.jamk.fi>
2014-10-01 12:10:00 +0300

    Harjoitukset

---
+++ harj3a.cpp
@@ -0,0 +1,22 @@
+/***********************************************
+*Harjoitus 3a
+*Jouni Ylönen
+*Kuvaus:
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nimeä tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan näytälle seuraavasti:
+*
+*Versio: 1.0
+*PVM: 3.9.2014
+************************************************/
+
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "Jouni Ylönen\n";
+	cout << "Vapaudenkatu 69 \n";
+}
---
+++ harj3b.cpp
@@ -0,0 +1,30 @@
+/***********************************************
+*Harjoitus 3b
+*Jouni Ylönen
+*Kuvaus:
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nimeä tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan näytälle seuraavasti:
+*Versio: 1.0
+*PVM: 3.9.2014
+************************************************/
+
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "**********************************\n"
+		<< "* *\n"
+		<< "* Etunimi *\n"
+		<< "* Sukunimi *\n"
+		<< "* *\n"
+		<< "*-------------------------------------- *\n"
+		<< "* *\n"
+		<< "* Vapaudenkatu 69 *\n"
+		<< "* 40520 JKL *\n"
+		<< "* *\n"
+		<< "**********************************\n";
+}
